862e57fd8a6e49dab81395601a7636c864b62175
MaksimAgliunin <maksim-aglyunin@mail.ru>
2021-02-02 11:56:24 -0800

    Final hosting version

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const config = require('config');
+const path = require('path')
 const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json({extended:true}));
+
 app.use('/api/auth', require('./routes/auth.routes')); //specifying routes
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = config.get('port') || 5000; // constant from config/default.json
 
